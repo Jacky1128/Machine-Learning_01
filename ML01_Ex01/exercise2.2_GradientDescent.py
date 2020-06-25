cae3c5986b452c57781825d92bea91cb211f09a3
Jacky1128 <jackywang28@outlook.com>
2020-06-25 21:03:04 +0800

    Update exercise2.2_GradientDescent.py

--- ML01_Ex01/exercise2.2_GradientDescent.py
+++ ML01_Ex01/exercise2.2_GradientDescent.py
@@ -48,31 +48,61 @@ print(theta.shape)
 # Calculate the cost function J(θ)
 print(computeCost(X,y,theta))
 
-## STEP_05: Achieve Gradient Descent
+## STEP_05: Define the Batch Gradient Descent Algorithm
 # NOTICE: The var of J(θ)isθ, not X or y. It means the guidline is we update the value of θ to update J(θ);
 # How to inspect if the algorithm is well-operating: Print the value of J(θ) in each step.
 # then see if it is decreasing and converge to a stable value.
 # The final result will be used to predict "小吃店在35000及70000人城市规模的利润"
 
-def gradientDescent(X, y, theta, alpha, iters):     # The defination of the gradient descent algorithm
-    temp = np.matrix(np.zeros(theta.shape))     #NOTICE: 用法：zeros(shape, dtype=float, order='C')
+def gradientDescent(X, y, theta, alpha, iters):     # The defination of Batch Gradient Descent algorithm
+    temp = np.matrix(np.zeros(theta.shape))     # Initialize a matrix filled with 0 (same scale with theta matrix)
+    #NOTICE: 用法：zeros(shape, dtype=float, order='C')
     #返回来一个给定形状和类型的用0填充的数组；
 
-    parameters = int(theta.ravel().shape[1])
-    cost = np.zeros(iters)
+    parameters = int(theta.ravel().shape[1])    # the number of parameter-θ
+    cost = np.zeros(iters)  # Initialize an array containing the value of J(θ) in each iteration
 
     for i in range(iters):
-        error = (X * theta.T) - y
+        error = (X * theta.T) - y   # to simplify the expression below
 
         for j in range(parameters):
-            term = np.multiply(error, X[:, j])
-            temp[0, j] = theta[0, j] - ((alpha / len(X)) * np.sum(term))
+            term = np.multiply(error, X[:, j])  # iteration step of θj, same as part of the iteration fomular
+            # NOTICE: method multiply():数组和矩阵对应位置相乘，输出与相乘数组/矩阵的大小一致
+            temp[0, j] = theta[0, j] - ((alpha / len(X)) * np.sum(term))    # same as the iteration fomular
 
-        theta = temp
-        cost[i] = computeCost(X, y, theta)
+        theta = temp    # same as the iteration fomular
+        cost[i] = computeCost(X, y, theta)  # Calculate J(θ)
 
     return theta, cost
 # this part achieves the update of Ѳ
+
+## STEP_06: Initialize 2 additional vars - Learning Rate α & number of iterations
+# let α be 0.01, iters be 1500
+alpha = 0.01
+iters = 1500
+
+## STEP_07: Run the algorithm, predict and draw the graph
+g, cost = gradientDescent(X, y, theta, alpha, iters)
+print(g)    # get the value of θ
+
+predict1 = [1,3.5]*g.T
+print("predict1:",predict1)
+predict2 = [1,7]*g.T
+print("predict2:",predict2)     # predict the profit of food stand with city scale in35000 & 70000 respectively
+
+x = np.linspace(data.Population.min(), data.Population.max(), 100)  # Set horizontal ordinates(population)
+f = g[0, 0] + (g[0, 1] * x)     # Set vertical ordinates(profit)
+
+fig, ax = plt.subplots(figsize=(12,8))
+ax.plot(x, f, 'r', label='Prediction')
+ax.scatter(data.Population, data.Profit, label='Traning Data')
+ax.legend(loc=2)
+ax.set_xlabel('Population')
+ax.set_ylabel('Profit')
+ax.set_title('Predicted Profit vs. Population Size')
+plt.show()
+#原始数据以及拟合的直线
+
 
 
 
